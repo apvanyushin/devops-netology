195f54b24468947b54e1b72733b0f12db8d5356c
apvanyushin <apvanyushin@yandex.ru>
2022-06-18 19:22:35 +0300

    terraform folder

---
+++ Виртуализация, базы данных и Terraform/terraform/main.tf
@@ -0,0 +1,36 @@
+provider "yandex" {
+  cloud_id  = "${var.yandex_cloud_id}"
+  folder_id = "${var.yandex_folder_id}"
+  zone      = "ru-central1-a"
+}
+resource "yandex_compute_instance" "vm-1" {
+  name = "tf"
+  resources {
+    cores  = 2
+    memory = 2
+  }
+  boot_disk {
+    initialize_params {
+      image_id = "fd8mn5e1cksb3s1pcq12"
+    }
+  }
+  network_interface {
+    subnet_id = yandex_vpc_subnet.subnet-1.id
+    nat       = true
+  }
+  metadata = {
+    ssh-keys = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
+  }
+}
+data "yandex_compute_image" "my_image"{
+  family = "ubuntu-2004-lts"
+}
+resource "yandex_vpc_network" "network-1" {
+  name = "network1"
+}
+resource "yandex_vpc_subnet" "subnet-1" {
+  name           = "subnet1"
+  zone           = "ru-central1-a"
+  network_id     = yandex_vpc_network.network-1.id
+  v4_cidr_blocks = ["192.168.10.0/24"]
+}
---
+++ Виртуализация, базы данных и Terraform/terraform/variables.tf
@@ -0,0 +1,6 @@
+variable "yandex_cloud_id" {
+  default = "b1guied7misid8fcvpfo"
+}
+variable "yandex_folder_id" {
+  default = "b1gvugh6rpqc2i1tmd8p"
+}
