2d321d915881bc04f6e477f7f12dc00a1d7f73e6
apvanyushin <apvanyushin@yandex.ru>
2022-06-19 19:13:32 +0300

    terraform cloud test

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,5 @@
 provider "yandex" {
-  token  = var.yandex_cloud_token
+#  token  = var.yandex_cloud_token
   cloud_id  = var.yandex_cloud_id
   folder_id = var.yandex_folder_id
   zone      = "ru-central1-a"
